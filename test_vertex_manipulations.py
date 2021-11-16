87d3a3c43ccffb49c971ac9172433c58c0fab2b2
Jurjen Verbruggen <jurjenverbruggen@gmail.com>
2021-11-16 10:23:52 +0100

    Rewrited basics

---
+++ test_vertex_manipulations.py
@@ -0,0 +1,52 @@
+from vertex_manipulations import *
+
+def test_degree():
+    assert degree(3, [(3,1), (1,2), (1,0), (2,4), (4,5)]) == 1
+    assert degree(4, [(3,1), (1,2), (1,0), (2,4), (4,5)]) == 2
+    assert degree(1, [(3,1), (1,2), (1,0), (2,4), (4,5)]) == 3
+    assert degree(6, [(3,1), (1,2), (1,0), (2,4), (4,5)]) == 0
+
+def test_find_adjacent_nodes():
+    assert find_adjacent_nodes(3, [(3,1), (1,2), (1,0), (2,4), (4,5)]) == [1]
+    assert find_adjacent_nodes(4, [(3,1), (1,2), (1,0), (2,4), (4,5)]) == [2,5]
+    assert find_adjacent_nodes(1, [(3,1), (1,2), (1,0), (2,4), (4,5)]) == [3,2,0]
+    assert find_adjacent_nodes(6, [(3,1), (1,2), (1,0), (2,4), (4,5)]) == []
+
+def test_random_node():
+    assert random_node([1,2,3,4]) in [1,2,3,4]
+    assert random_node([1,2,3,4]) in [1,2,3,4]
+    assert random_node([1,2,3,4]) in [1,2,3,4]
+    assert random_node([1,2,3,4]) in [1,2,3,4]
+
+    assert random_node([1,2,3,4], not_in=[1,2]) in [3,4]
+    assert random_node([1,2,3,4], not_in=[1,2]) in [3,4]
+    assert random_node([1,2,3,4], not_in=[1,2]) in [3,4]
+    assert random_node([1,2,3,4], not_in=[1,2]) in [3,4]
+
+def test_kernalize_vertices():
+    (kernalized_vertices, kernalized_edges, sure_in_cover, k) = kernalize_vertices(
+        [1,2,3,4,5],
+        [(1,2), (1,3), (1,4), (1,5), (2,3), (2,5), (3,5), (3,4), (4,5)], k=3)
+
+    assert sure_in_cover == [1,3,5]
+    assert kernalized_vertices == []
+    assert kernalized_edges == []
+    assert k == 0
+
+    (kernalized_vertices, kernalized_edges, sure_in_cover, k) = kernalize_vertices(
+        [1,2,3,4,5],
+        [(1,2), (1,3), (1,4), (1,5), (2,3), (2,4), (2,5), (3,5), (3,4), (4,5)], k=3)
+
+    assert sure_in_cover == None
+    assert kernalized_vertices == None
+    assert kernalized_edges == None
+    assert k == None
+    
+    (kernalized_vertices, kernalized_edges, sure_in_cover, k) = kernalize_vertices(
+        [0,1,2,3,4,5,6],[(0,2),(0,3),(0,6),(0,1),(1,2),(1,3),(1,6),(1,4),(1,5),(2,3),(2,6),(3,6),(4,6),(5,6)], k=4)
+
+    assert sure_in_cover == [1,6]
+    assert kernalized_vertices == [0,2,3]
+    assert kernalized_edges == [(0,2),(0,3),(2,3)]
+    assert k == 2
+    
---
+++ vertex_manipulations.py
@@ -0,0 +1,81 @@
+from random import randrange
+
+def degree(node: int, edges: list[(int, int)]) -> int:
+    deg = 0
+    for (ea, eb) in edges:
+        if ea == node:
+            deg += 1
+        if eb == node:
+            deg += 1
+    return deg
+
+def find_adjacent_nodes(node: int, edges: list[(int, int)]) -> list[int]:
+    nodes = []
+
+    for (ea, eb) in edges:
+        if ea == node:
+            nodes += [eb]
+        if eb == node:
+            nodes += [ea]
+
+    return nodes
+
+def find_isolated_nodes(nodes: list[int], edges: list[(int, int)]) -> list[int]:
+    isolated = []
+    for node in nodes:
+        if degree(node, edges) == 0:
+            isolated += [node]
+    return isolated
+
+def find_pendant_nodes(nodes: list[int], edges: list[(int, int)]) -> list[int]:
+    pendant = []
+    for node in nodes:
+        if degree(node, edges) == 1:
+            pendant += [node]
+    return pendant
+
+def find_tops_nodes(nodes: list[int], edges: list[(int, int)], k: int) -> list[int]:
+    tops = []
+    for node in nodes:
+        if degree(node, edges) > k:
+            tops += [node]
+            k -= 1
+    return tops
+
+def random_node(nodes: list[int], not_in: list[int] = []) -> int:
+    working = [n for n in nodes if n not in not_in]
+    return working[randrange(len(working))]
+
+def remove_edges_for(node: int, edges: list[(int, int)]) -> list[(int, int)]:
+    return [(ea, eb) for (ea, eb) in edges if ea != node and eb != node]
+
+def kernalize_vertices(nodes: list[int], edges: list[(int, int)], k) -> (list[int], list[(int, int)], list[int], int):
+    result_nodes = nodes[:]
+    result_edges = edges[:]
+    nodes_surely_in_cover = []
+
+    i = 0
+    len_nodes = len(nodes)
+    looping = True
+
+    while looping:
+        looping = False
+        for node in result_nodes:
+            deg = degree(node, result_edges)
+            if k > 0 and deg > k:
+                k -= 1
+                nodes_surely_in_cover += [node]
+                result_nodes.remove(node)
+                result_edges = remove_edges_for(node, result_edges)
+                looping = True
+            elif deg == 0:
+                result_nodes.remove(node)
+                looping = True
+        if looping == False and len(result_edges) > k ** 2:
+            result_nodes = result_edges = nodes_surely_in_cover = None
+            break
+
+    if result_nodes is None:
+        return (None, None, None, None)
+
+    return (result_nodes, result_edges, sorted(nodes_surely_in_cover), k)
